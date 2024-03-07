da02f8b7ec64aab81bde93e1eb0a04f70c418854
Benoit Joly <benoit@benoitj.ca>
2024-03-06 23:23:33 -0500

    flakeify

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: 
+    let pkgs = import nixpkgs { system = "x86_64-linux";
+                                config.allowUnfree = true;
+                                overlays = [];
+                              }; 
+    in {
+    
+      devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = with pkgs; [
+          yubikey-manager
+        ];
+      };
+    };
+}
